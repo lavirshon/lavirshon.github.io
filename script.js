2af9576529577f6412ff1e62b4c5954059a86b53
lavirshon <lavirshon@gmail.com>
2016-07-19 16:38:06 +0200

    collapse jvs

--- script.js
+++ script.js
@@ -15,3 +15,23 @@ $(function() {
     }
   });
 });
+
+
+var header = document.getElementById("header");
+// Attach a listener to the window
+window.onscroll = function(e) {
+  
+  // Get the pageYOffset
+  var pageOffset = window.pageYOffset;
+  
+  // If the offset is more than 200
+  if ( pageOffset > 200 ) {
+    // Add the class name
+    header.classList.add("collapsed");
+  // If the offset is less than 200 
+  } else {
+    // Remove the class name
+    header.classList.remove("collapsed");   
+  };
+  
+};
